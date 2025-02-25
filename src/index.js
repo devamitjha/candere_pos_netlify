29ea28e2ccab23951ce287b8e8c0a3d11c5aa687
amitjha25in <amit.jha@candere.com>
2025-02-25 15:34:02 +0530

    new app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {store} from "./store/Store"
@@ -10,11 +10,11 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
           <ToastContainer position="top-right" closeOnClick/>
           <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 reportWebVitals();
